a3e4075b33361c1b3721b2f5a10bcb9aa3f8cc56
João Victor Veronesi <36904929+jtito27@users.noreply.github.com>
2018-06-04 12:37:39 -0300

    Entrega 2 - IONIC

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+import {ListaPaisesPage} from '../pages/lista-paises/lista-paises';
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListaPaisesPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListaPaisesPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
